23b7387c1fc926fc2827ad330d499c31ba3f1221
ania0 <ania000535@gmail.com>
2023-12-19 14:15:47 +0100

    add

--- Leson_67_Callable_Runnable_Futhure/src/Homework/Task2/Variant2.java
+++ Leson_67_Callable_Runnable_Futhure/src/Homework/Task2/Variant2.java
@@ -1,6 +1,6 @@
 package Homework.Task2;
 
-public class Variant1 extends Thread {
+public class Variant2 extends Thread {
     //*** Есть три потока.
     // 1 поток выводит в консоль пять раз букву А (без переноса строки)
     // 2 поток выводит в консоль пять раз букву В (без переноса строки)
@@ -12,31 +12,46 @@ public class Variant1 extends Thread {
 
     private static String data = "";
 
-    public static void run(String litera) {
-        for (int i = 0; i < 5; i++) {
-            synchronized (data) {
-                try {
-                    data.notify();
-                    data.wait(5);
-                    System.out.print(litera);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
-
-
     public static void main(String[] args) {
 
         Runnable runnable1 = () -> {
-            run("A");
+            for (int i = 0; i < 5; i++) {
+                synchronized (data) {
+                    try {
+                        data.notifyAll();
+                        data.wait(5);
+                        System.out.print("A");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            };
         };
         Runnable runnable2 = () -> {
-            run("B");
+            for (int i = 0; i < 5; i++) {
+                synchronized (data) {
+                    try {
+                        data.notifyAll();
+                        data.wait(5);
+                        System.out.print("B");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         };
         Runnable runnable3 = () -> {
-            run("C");
+            for (int i = 0; i < 5; i++) {
+                synchronized (data) {
+                    try {
+                        data.notifyAll();
+                        data.wait(5);
+                        System.out.print("C");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         };
 
         Thread thread1 = new Thread(runnable1);
